5df09d3c3d92643c0dda6c0b792fe9bcf3f526b2
shitalRandhave <shitalrandhave4@gmail.com>
2021-08-23 02:55:42 +0530

    Adding up the project firstly over git

---
+++ src/test/java/Session_1/DataTypes_1.java
@@ -0,0 +1,13 @@
+package Session_1;
+
+public class DataTypes_1 {
+
+	public static void main(String[] args) 
+	{
+		int height;
+		height = 10;
+		System.out.println("the height of a person is - " + height);
+
+	}
+
+}
